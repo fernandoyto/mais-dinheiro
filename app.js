f9a91b32b401837eee161702e4417905c7f50b83
Fernando Yto <fernando.toshioyto@gmail.com>
2019-09-13 22:08:51 -0300

    <feat>(front)(back): created login view, login post route, new dependencies for user session control

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const hbs = require('hbs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
@@ -16,20 +19,37 @@ mongoose.connect('mongodb://localhost/maisDinheiro', { useNewUrlParser: true })
     console.log(`Error connecting to Mongo: ${err}`);
   });
 
-const landingPage = require('./routes/landingPage');
-const authRoutes = require('./routes/authRoutes');
+// Middleware setup
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60,
+  }),
+}));
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true,
+}));
 
 // Make everything inside of public/ available
 app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // creates an absolute path pointing to a folder called "views"
 app.set('views', __dirname + '/views');
 // tell our Express app that HBS will be in charge of rendering the HTML:
 app.set('view engine', 'hbs');
 
-//makes a req when the user go to the route /
+// Makes a req when the user go to the route /
+const landingPage = require('./routes/public/landingPage');
+
+const authRoutes = require('./routes/public/authRoutes');
+
 app.use('/', landingPage);
 app.use('/', authRoutes);
 
